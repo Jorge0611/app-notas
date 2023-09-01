a7f825268cc0df134b2b04e4cb0859e5f925a6f9
vicjoel13 <vicjoel13@gmail.com>
2023-09-01 15:18:13 -0400

    probando

--- seedNotes.js
+++ seedNotes.js
@@ -1,33 +1,33 @@
-const mongoose = require("mongoose");
-const Notes = require("./database"); // Importa el modelo de Notas
-const data = require("./notes.json");
+const mongoose = require('mongoose');
+const Notes = require('./database'); // Importa el modelo de Notas
+const data = require('./notes.json');
 
-const { MONGO_DB_USR, MONGO_DB_PWD, MONGO_DB_HOST, MONGO_DB_PORT } =
+const {MONGO_DB_USR, MONGO_DB_PWD, MONGO_DB_HOST, MONGO_DB_PORT} =
   process.env;
-const credentials = MONGO_DB_USR ? `${MONGO_DB_USR}:${MONGO_DB_PWD}@` : "";
+const credentials = MONGO_DB_USR ? `${MONGO_DB_USR}:${MONGO_DB_PWD}@` : '';
 const mongoURI = `mongodb://${credentials}${MONGO_DB_HOST}:${MONGO_DB_PORT}/`;
 
 const seedDatabase = async () => {
   try {
-
-    await mongoose.connect("mongodb://localhost:27017", {
+    await mongoose.connect('mongodb://localhost:27017', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
 
-    // Borra todos los documentos existentes en la colección "notes"
+    // Borra todos los documentos existentes en la colección 'notes'
     await Notes.deleteMany({});
 
-    // Inserta los datos de "notes.json" en la base de datos utilizando el modelo Notes
+    // Inserta los datos de 'notes.json' en la base de datos utilizando
+    // el modelo Notes
     await Notes.insertMany(data);
 
-    console.log("Carga de datos de prueba completa!");
-    console.log("Por favor, refresca la página.");
+    console.log('Carga de datos de prueba completa!');
+    console.log('Por favor, refresca la página.');
 
     // Cierra la conexión a la base de datos
     mongoose.connection.close();
   } catch (error) {
-    console.error("Error al cargar los datos de prueba:", error);
+    console.error('Error al cargar los datos de prueba:', error);
   }
 };
 
